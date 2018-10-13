dcc5d95b40de8f98e18125db0857c8cd0d4b335e
sumaris <alfred.radu@gmail.com>
2018-10-14 01:55:23 +0200

    added dummy pause menu, accessed by pressing esc in game

--- GameTitle/Main.gd
+++ GameTitle/Main.gd
@@ -7,3 +7,12 @@ var screensize
 func _ready():
 	screensize = get_viewport_rect().size
 	self.position = screensize/2
+	set_process_unhandled_input(true)
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			if !$pause.is_visible():
+				$pause.show()
+			else:
+				$pause.hide()
