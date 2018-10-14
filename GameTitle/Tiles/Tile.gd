eac87c8a5f2c7c3c545ba0800e0e786489812311
Ekin Inceleme <einceleme@gmail.com>
2018-10-14 20:02:52 +0100

    Can now inherit from Tile.tscn for new tiles. Default is no doors

--- GameTitle/Tiles/Tile.gd
+++ GameTitle/Tiles/Tile.gd
@@ -9,60 +9,77 @@ var Door = preload("res://Tiles/Door.tscn")
 var doors
 var TILE_WIDTH = 512
 
-export(bool) var UpperDoor = false
-export(bool) var RightDoor = false
-export(bool) var LeftDoor = false
-export(bool) var LowerDoor = false
+export(bool) var upper_door setget upper_door_set, upper_door_get
+export(bool) var right_hand_door setget right_hand_door_set, right_hand_door_get
+export(bool) var lower_door setget lower_door_set, lower_door_get
+export(bool) var left_hand_door setget left_hand_door_set, left_hand_door_get
+
+var upper_door_instance
+var right_hand_door_instance
+var lower_door_instance
+var left_hand_door_instance
 
 var Grid	# Again we should avoid using parent objects like this in the future - have the grid object do things itself
 
-func _ready():
+func _init():
 	self.doors = []
-	
-	var upper_door = Door.instance()
-	var right_hand_door = Door.instance()
-	var lower_door = Door.instance()
-	var left_hand_door = Door.instance()
-	
-	upper_door.door_pos = Vector2(0, -1)
-	right_hand_door.door_pos = Vector2(1, 0)
-	lower_door.door_pos = Vector2(0, 1)
-	left_hand_door.door_pos = Vector2(-1, 0)
-  
-	self.add_door(upper_door)
-	self.add_door(right_hand_door)
-	self.add_door(lower_door)
-	self.add_door(left_hand_door)
+
+func _ready():
 	
 	self.Grid = self.get_parent()
 	self.connect("tile_constructed", Grid, "_on_tile_constructed", [self])
 	emit_signal("tile_constructed")
 
-#func _process(delta):
-#	if UpperDoor:
-#		var upper_door = Door.instance()
-#		upper_door.door_pos = Vector2(0, -1)
-#		self.add_door(upper_door)
-#		UpperDoor = int(UpperDoor) - 1
-#	if RightDoor:
-#		var right_hand_door = Door.instance()
-#		right_hand_door.door_pos = Vector2(1, 0)
-#		self.add_door(right_hand_door)
-#		RightDoor = int(RightDoor) - 1
-#	if LeftDoor:
-#		var left_hand_door = Door.instance()
-#		left_hand_door.door_pos = Vector2(-1, 0)
-#		self.add_door(left_hand_door)
-#		LeftDoor = int(LeftDoor) - 1
-#	if LowerDoor:
-#		var lower_door = Door.instance()
-#		lower_door.door_pos = Vector2(0, 1)
-#		self.add_door(lower_door)
-#		LowerDoor = int(LowerDoor) - 1
+# WARNING WARNING GABAGE CODE AHEAD. 
+# I had to copy and paste the below right now, need something better.
+
+func upper_door_set(new_bool):
+	if !self.upper_door && new_bool:
+		self.upper_door_instance = Door.instance()
+		self.upper_door_instance.door_pos = Vector2(0, -1)
+		self.add_door(self.upper_door_instance)
+	elif self.upper_door && !new_bool:
+		self.upper_door_instance.queue_free()
+	
+func upper_door_get(new_bool):
+	return self.upper_door_instance != null
+	
+func right_hand_door_set(new_bool):
+	if !self.right_hand_door && new_bool:
+		self.right_hand_door_instance = Door.instance()
+		self.right_hand_door_instance.door_pos = Vector2(1, 0)
+		self.add_door(self.right_hand_door_instance)
+	elif self.right_hand_door && !new_bool:
+		self.right_hand_door_instance.queue_free()
+	
+func right_hand_door_get(new_bool):
+	return self.right_hand_door_instance != null
+	
+func lower_door_set(new_bool):
+	if !self.lower_door && new_bool:
+		self.lower_door_instance = Door.instance()
+		self.lower_door_instance.door_pos = Vector2(0, 1)
+		self.add_door(self.lower_door_instance)
+	elif self.lower_door && !new_bool:
+		self.lower_door_instance.queue_free()
+	
+func lower_door_get(new_bool):
+	return self.lower_door_instance != null
+	
+func left_hand_door_set(new_bool):
+	if !self.left_hand_door && new_bool:
+		self.left_hand_door_instance = Door.instance()
+		self.left_hand_door_instance.door_pos = Vector2(-1, 0)
+		self.add_door(self.left_hand_door_instance)
+	elif self.left_hand_door && !new_bool:
+		self.left_hand_door_instance.queue_free()
+	
+func left_hand_door_get(new_bool):
+	return self.left_hand_door_instance != null
 
 func add_door(door):
-	self.add_child(door)
 	self.doors.append(door)
+	self.add_child(door)
 
 func remove_door(door):
 	self.doors.remove(doors.find(door))
