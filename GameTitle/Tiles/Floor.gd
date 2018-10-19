f7e879bfef5ab67694095a0d9c5df92d1bd85e1b
awindahl <awindahl@kth.se>
2018-10-19 23:41:03 +0200

    added treasury tile, fixed small bug in hallway

--- GameTitle/Tiles/Floor.gd
+++ GameTitle/Tiles/Floor.gd
@@ -16,10 +16,11 @@ var WinecellarTile = preload("res://Tiles/WinecellarTile.tscn")
 var ChapeTile = preload("res://Tiles/ChapelTile.tscn")
 var ArtgalleryTile = preload("res://Tiles/ArtgalleryTile.tscn")
 var GuestbedroomTile = preload("res://Tiles/GuestbedroomTile.tscn")
+var TreasuryTile = preload("res://Tiles/TreasuryTile.tscn")
 
 var tile_list = [HallwayTile, BasicTile, CrossingTile, BallroomTile, KitchenTile, StorageTile, BedroomTile,
 		StairwayTile, DiningroomTile, LaundromatTile, OldpassageTile, WinecellarTile, ChapeTile, ArtgalleryTile,
-		GuestbedroomTile]
+		GuestbedroomTile, TreasuryTile]
 
 var tiles
 
